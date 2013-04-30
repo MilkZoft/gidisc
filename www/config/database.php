5d3915c70a9798f9cb1822b2cee31b18689df10f
cris <cris@cris.(none)>
2013-04-29 20:53:03 -0500

    Configs

--- www/config/database.php
+++ www/config/database.php
@@ -13,8 +13,8 @@ if(!defined("_access")) {
 $ZP["db"]["dbController"] = "mysqli";
 $ZP["db"]["dbHost"] 	  = "localhost";
 $ZP["db"]["dbUser"] 	  = "root";
-$ZP["db"]["dbPwd"] 	      = "";
-$ZP["db"]["dbName"] 	  = "gidi";
+$ZP["db"]["dbPwd"] 	      = "jesusamor";
+$ZP["db"]["dbName"] 	  = "gidisc";
 $ZP["db"]["dbPort"] 	  = 3306;
 $ZP["db"]["dbPfx"] 	      = "zan_";
 $ZP["db"]["dbSocket"]     = NULL;
