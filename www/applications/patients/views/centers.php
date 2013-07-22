8757652077f990467979c31f1d9db3b2a4a90a79
Carlos Santana <carlos@milkzoft.com>
2013-07-21 19:51:45 -0500

    Home

---
+++ www/applications/patients/views/centers.php
@@ -0,0 +1,28 @@
+<form action="<?php echo path("patients/centers/$IDPatient"); ?>" method="post">
+	<?php
+		$checked = NULL;
+		die(var_dump($all));
+		foreach($all as $user) { 			
+ 			foreach($already as $person) {
+ 				if($person["ID_User"] === $user["ID_User"]) { 
+ 					$checked = ' checked="checked"';
+ 				} 
+ 			} 
+		?>
+			<input<?php echo $checked; ?> name="users[]" type="checkbox" value="<?php echo $user["ID_User"]; ?>" /> <?php echo $user["Username"]; ?> (<?php echo $type; ?>)<br />
+		<?php
+
+			$checked = NULL;
+		}
+	?>
+	<br />
+	<input name="ID_Patient" value="<?php echo $IDPatient; ?>" type="hidden" />
+	
+	<p>
+		<input name="assign" value="Asignar Permisos" class="btn primary" type="submit" />
+	</p>
+
+	<p>
+		<a href="<?php echo path("patients"); ?>">Regresar</a>
+	</p>
+</form>
